f44493394b93f607ff3a7277ebe13b795e600102
chenzdido <chenzdido@gmail.com>
2020-07-30 16:31:54 +0800

    [zhe] feat: complete GrammarExercise.java

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,38 @@
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
         //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+        Scanner input = new Scanner(System.in);
+        String firstWordList = input.nextLine();
+        String secondWordList = input.nextLine();
 
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
         //按要求输出到命令行
+        result.forEach(System.out::println);
 
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        List<String> fwl=Stream.of(firstWordList.split(",")).map(i->i.toUpperCase()).distinct().collect(Collectors.toList());
+        List<String> swl=Stream.of(secondWordList.split(",")).map(i->i.toUpperCase()).distinct().collect(Collectors.toList());
+        fwl.forEach(it->{
+            if(!it.matches("[a-zA-Z]")){
+                throw new RuntimeException("input not valid");
+            }
+        });
+        swl.forEach(it->{
+            if(!it.matches("[a-zA-Z]")){
+                throw new RuntimeException("input not valid");
+            }
+        });
+        List res=fwl.stream().filter(it->swl.contains(it)).collect(Collectors.toList());
+        res.stream().sorted();
+
+        return res;
     }
 }
